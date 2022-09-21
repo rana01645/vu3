2388ef968bd4bd2b1f6ee2c515ed9b3511a7caf3
rana01645 <rana01645@gmail.com>
2022-09-21 15:16:24 +0600

    wip

---
+++ src/components/ObjecVModel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+
+
+    <input v-model="form.name" type="text">
+    <input v-model="form.email" type="text">
+    <input v-model="form.password" type="password">
+
+  </div>
+</template>
+
+<script setup>
+import {defineProps,defineEmits,computed} from "vue";
+
+const  props = defineProps({
+  modelValue: Object
+})
+
+//create a method named update
+const emit = defineEmits(['update:modelValue'])
+const form = computed({  // Use computed to wrap the object
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+});
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
